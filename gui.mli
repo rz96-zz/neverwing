211b4e64937fb5e8f1003cb5a2b80a281750d5a0
Alice Zhou <rz96@cornell.edu>
2017-11-05 15:23:01 -0500

    draw_char

--- gui.mli
+++ gui.mli
@@ -1,4 +1,8 @@
 (*Draw score GUI*)
 val draw_score: Dom_html.canvasRenderingContext2D Js.t -> unit
+
 (*Draw game over GUI*)
 val game_over: Dom_html.canvasRenderingContext2D Js.t -> unit
+
+(*Draw characters*)
+val draw_char: Character.sprite -> float * float-> unit
