4212466ee26271b47a4e9328e7697d945da7e420
Alice Zhou <rz96@cornell.edu>
2017-11-05 15:12:01 -0500

    key down key up

--- command.mli
+++ command.mli
@@ -1,4 +1,11 @@
-type control =
-  |Left
-  |Right
-  |Pause
+(* Initiates the main game loop
+val update_loop : Dom_html.canvasElement Js.t
+-> (Object.collidable * Object.collidable list)
+-> float*float
+-> unit *)
+
+(* Keydown event handler function for key pressed*)
+val key_down : #Dom_html.keyboardEvent Js.t -> bool Js.t
+
+(* Keyup event handler function for key unpressed*)
+val key_up : #Dom_html.keyboardEvent Js.t -> bool Js.t
