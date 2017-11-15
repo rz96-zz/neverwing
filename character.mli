c81051e6014bff5462098a327501ae74e3e18003
mrong98 <morenarong@dhcp-hol-126.eduroam.cornell.edu>
2017-11-15 15:03:28 -0500

    some specs more specified

--- character.mli
+++ character.mli
@@ -1,15 +1,17 @@
 (*characters are moveable items*)
 
+(*Type direction corresponds to the direction the character moves in,
+either Right or Left*)
 type direction = | Right | Left
 
-(*Gives xy coordinates of position*)
+(*Gives xy coordinates of a position on the board*)
 type xy = {
   mutable x: float;
   mutable y: float;
 }
 
 (*Command corresponds to player keyboard input*)
-type command = 
+type command =
   | CLeft
   | CRight
   | CPause
--- gui.mli
+++ gui.mli
@@ -1,11 +1,11 @@
 open State
 open Character
 
-(*Draw score GUI*)
+(*Draw the player's score in the GUI*)
 val draw_score: Dom_html.canvasRenderingContext2D Js.t -> unit
 
-(*Draw game over GUI*)
+(*Draw "game over" state in the GUI*)
 val game_over: Dom_html.canvasRenderingContext2D Js.t -> unit
 
-(*Draw characters*)
+(*Draw characters in the GUI*)
 val draw_char: Character.sprite -> float * float-> unit
