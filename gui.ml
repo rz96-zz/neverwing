c63281e08830d808dbf12325fec061c8d666feaa
Alice Zhou <rz96@cornell.edu>
2017-11-17 15:50:18 -0500

    gui skeleton and spirite

---
+++ gui.ml
@@ -0,0 +1,9 @@
+let draw_score = failwith "unimplemented"
+
+let game_over = failwith "unimplemented"
+
+let draw_char = failwith "unimplemented"
+
+let fps = failwith "unimplemented"
+
+let draw_bgd = failwith "unimplemented"
---
+++ sprite.ml
@@ -0,0 +1,1 @@
+type sprite = unit 
