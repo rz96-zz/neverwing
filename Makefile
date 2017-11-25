b3f0e98f3c7a06a72dda11385da445253a4577e9
saf243 <saf243@cornell.edu>
2017-11-24 22:55:58 -0500

    makefile works

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
-all: byte
-	js_of_ocaml main2.byte
 
-byte:
-	ocamlbuild -use-ocamlfind -pkgs js_of_ocaml-lwt,js_of_ocaml.ppx,lwt.ppx main2.byte
+OBJS= sprite.cmo character.cmo board.cmo gui.cmo state.cmo command.cmo
+NAME=main2
+OCAMLC=ocamlfind ocamlc -thread -package js_of_ocaml-lwt -package js_of_ocaml.ppx -package lwt.ppx
+$(NAME).byte: $(OBJS)
+	$(OCAMLC) -linkpkg -o $@ $(OBJS) $(NAME).ml
+
+$(NAME).js: $(NAME).byte
+	js_of_ocaml $<
+
+%.cmo: %.ml
+	$(OCAMLC) -c $<i
+	$(OCAMLC) -c $<
--- sprite.ml
+++ sprite.ml
@@ -1,7 +1,7 @@
 type width_height = float * float
 type x_y = float * float
 
-type sprite_props = 
+type sprite_props =
   {
     img_src: string;
     frame_size: width_height;
@@ -14,7 +14,7 @@ type sprite = {
   mutable img: Dom_html.imageElement Js.t;
 }
 
-let init_sprite img_src frame_size frame_offset = 
+let init_sprite img_src frame_size frame_offset =
   let img_src = "./sprites/" ^ img_src in
   {
     img_src;
@@ -22,15 +22,16 @@ let init_sprite img_src frame_size frame_offset =
     frame_offset;
   }
 
-let make_sprite prop context = 
-  let img = (Dom_html.createImg Dom_html.document) in
+let make_sprite prop context =
+  (*let img = (Dom_html.createImg Dom_html.document) in
   img##src <- (Js.string prop.img_src);
-  { 
+  {
     prop;
     context;
     img;
-  }
+    }*)
+  failwith "Unimplemented"
 
-let init_bgd context = 
-  let prop = init_sprite "bgd.png" (500., 600.) (0., 0.) in 
+let init_bgd context =
+  let prop = init_sprite "bgd.png" (500., 600.) (0., 0.) in
   make_sprite prop context
