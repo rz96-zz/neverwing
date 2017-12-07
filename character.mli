aeb944ea9619d7511e833e6ed16c5258a94464c6
jillwuu <jillwuu@users.noreply.github.com>
2017-12-07 13:19:59 -0500

    comments?

---
+++ character.mli
@@ -0,0 +1,41 @@
+(*characters are moveable items*)
+open Sprite
+open Board
+
+(*Type direction corresponds to the direction the character moves in,
+either Right or Left*)
+(*type direction = | Right | Left
+
+(*Gives xy coordinates of a position on the board*)
+type xy = float * float
+
+(*Command corresponds to player keyboard input*)
+type command =
+  | CLeft
+  | CRight
+  | CPause
+  | None*)
+
+      (*)(*Type monsters represents the different types of monsters
+there are in the game*)
+type monster =
+  | Boss
+  | Medium
+  | Small
+
+(*Type damage represents the objects that could inflict damage
+on a player*)
+type damage =
+  | Boss_Projectile
+  | Monster
+
+(*Type object is any object that appears on the board*)
+type obj = {
+  sprite: Sprite.sprite;
+  pos: xy;
+}*)
+
+(*val new_obj : Sprite.sprite_props -> Dom_html.canvasRenderingContext2D Js.t
+  -> xy -> obj*)
+
+val check_collision: obj option -> obj option -> bool
